fc8c810a7bc359faec7ed9554a489150b667841b
Mateusz Woźniak <matisiek11@gmail.com>
2025-10-15 20:33:46 +0200

    feat: inject services

--- src/service/application_service.cpp
+++ src/service/application_service.cpp
@@ -3,28 +3,53 @@
 #include <qstring.h>
 
 #include "../../include/repository/signal_repository.h"
+#include "../../include/service/filter_service.h"
+#include "../../include/service/r_peaks_detection_service.h"
 
 class ApplicationService : public IApplicationService {
-    std::shared_ptr<ISignalRepository> signalRepo_;
+    std::shared_ptr<ISignalRepository> signal_repository_;
+    std::shared_ptr<IFilterService> butterworth_filter_service_;
+    std::shared_ptr<IFilterService> moving_average_filter_service_;
+    std::shared_ptr<IRPeaksDetectionService> r_peaks_detection_service_;
 
 public:
-    explicit ApplicationService(std::shared_ptr<ISignalRepository> repo)
-        : signalRepo_(std::move(repo)) {
+    explicit ApplicationService(
+        std::shared_ptr<ISignalRepository> signal_repository,
+        std::shared_ptr<IFilterService> butterworth_filter_service,
+        std::shared_ptr<IFilterService> moving_average_filter_service,
+        std::shared_ptr<IRPeaksDetectionService> r_peaks_detection_service
+
+    )
+        : signal_repository_(std::move(signal_repository)),
+          butterworth_filter_service_(std::move(butterworth_filter_service)),
+          moving_average_filter_service_(std::move(moving_average_filter_service)),
+          r_peaks_detection_service_(std::move(r_peaks_detection_service)) {
     }
 
-    bool Load(QString filename) override{}
+    bool Load(QString filename) override {
+        signal_repository_->Load(filename);
+    }
 
-    int GetLength() override{}
+    int GetLength() override {
+    }
 
-    SignalRange GetViewRange() override{}
+    SignalRange GetViewRange() override {
+    }
 
-    void SetViewRange(SignalRange range) override{}
+    void SetViewRange(SignalRange range) override {
+    }
 
-    std::shared_ptr<std::vector<SignalDatapoint>> GetData() override{}
+    std::shared_ptr<std::vector<SignalDatapoint> > GetData() override {
+    }
 
-    std::shared_ptr<std::vector<SignalDatapoint>> GetFilteredData() override{}
+    std::shared_ptr<std::vector<SignalDatapoint> > GetFilteredData() override {
+    }
 
-    Status GetStatus() override{}
+    Status GetStatus() override {
+    }
 
-    bool RunFiltering(FilterMethod method) override{}
+    bool RunFiltering(FilterMethod method) override {
+    }
+
+    int GetFrequency() override {};
 };
