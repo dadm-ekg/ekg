9818cad5c734ba512c35d54c43a82d1e643b82ee
Mateusz Woźniak <matisiek11@gmail.com>
2025-10-15 20:27:01 +0200

    feat: r peaks detection service

---
+++ include/service/r_peaks_detection_service.h
@@ -0,0 +1,16 @@
+#ifndef EKG_R_PEAKS_SERVICE_H
+#define EKG_R_PEAKS_SERVICE_H
+#include <vector>
+
+#include "../model/r_peaks_annotated_signal_datapoint.h"
+#include "../model/signal_datapoint.h"
+
+class IRPeaksDetectionService {
+public:
+    virtual ~IRPeaksDetectionService() = default;
+
+    virtual std::vector<RPeaksAnnotatedSignalDatapoint> Detect(std::vector<SignalDatapoint> datapoints, int frequency) =
+    0;
+};
+
+#endif //EKG_R_PEAKS_SERVICE_H
---
+++ src/service/r_peaks_detection_service.cpp
@@ -0,0 +1,9 @@
+#include "../../include/service/r_peaks_detection_service.h"
+
+class RPeaksDetectionService : public IRPeaksDetectionService {
+public:
+    std::vector<RPeaksAnnotatedSignalDatapoint>
+    Detect(std::vector<SignalDatapoint> datapoints, int frequency) override {
+        // TODO(Wiktor Raczek): trzeba uzupełnić. Ta metoda musi zwrócić wektor RPeaksAnnotatedSignalDatapoint z uzupełnionymi wartościami RPeaksAnnotatedSignalDatapoint.peak = true, jeżeli w danym punkcie występuje górka
+    };
+};
