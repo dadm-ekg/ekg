0b02a5d5471441c8be431614f270cf00c3a4624d
Mateusz Woźniak <matisiek11@gmail.com>
2025-10-15 20:23:13 +0200

    feat: comments on filtering services

---
+++ src/service/moving_average_filter_service.cpp
@@ -0,0 +1,8 @@
+#include "../../include/service/filter_service.h"
+
+class MovingAverageFilterService : public IFilterService {
+public:
+    std::vector<SignalDatapoint> Filter(std::vector<SignalDatapoint> values) override {
+        // TODO(Ola Szota): trzeba uzupełnić
+    }
+};
