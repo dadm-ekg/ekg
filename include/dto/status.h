97d01950c89b04d7c3935958084ee7c7159a27bb
Mateusz Woźniak <matisiek11@gmail.com>
2025-10-15 20:23:13 +0200

    feat: comments on filtering services

--- include/dto/status.h
+++ include/dto/status.h
@@ -2,9 +2,13 @@
 #define EKG_STATUS_H
 
 enum Status {
+    // Domyślny stan programu, oznacza, że żaden plik nie jest załadowany
     idle,
+    // Stan w którym dane są ładowane
     loading,
+    // Stan, w którym dane w GetData() są dostępne (ale nadal GetFilteredData() może zwracać NULLPTR)
     loaded,
+    // Tymczasowy stan, który informuje, że wykonywane jest jakieś obliczenie (filtrowanie, wykrywanie itp.)
     processing
 };
 
--- include/service/application_service.h
+++ include/service/application_service.h
@@ -10,19 +10,36 @@ class IApplicationService {
 public:
     virtual ~IApplicationService() = default;
 
+    // Ta metoda służy do podstawowego załadowania pliku za pomocą CSV
     virtual bool Load(QString filename) = 0;
 
+    // Ta metoda zwraca długość wektora wartości
     virtual int GetLength() = 0;
 
+    // Ta metoda zwraca częstotliwość zbioru danych w hertzach
+    virtual int GetFrequency() = 0;
+
+    // W aplikacji istnieje koncept ViewRange - jest to zakres aktualnie wyświetlanego wykresu sygnału
+    // SignalRange.start wskazuje od którego indeksu wektora powinny być wyświetlane dane
+    // SignalRange.end wskazuje końcowy indeks
     virtual SignalRange GetViewRange() = 0;
 
+    // Użytkownik powinien mieć możliwość ustawienia zakresu w trakcie korzystania z programu
     virtual void SetViewRange(SignalRange range) = 0;
 
+    // Zwraca sygnał w określonym zakresie przez SignalRange
+    // Ta metoda powinna być użyta do wyrysowania wykresu sygnału
+    // Ten sygnał nie jest przefiltrowany. W przypadku gdy Status=idle, ta metoda zwraca NULLPTR
     virtual std::shared_ptr<std::vector<SignalDatapoint>> GetData() = 0;
+    // Metoda działająca analogicznie do GetData(), przy czym zwraca sygnał przefiltrowany.
+    // Jeżeli użytkownik nie uruchomił żadnego algorytmu filtracji, metoda zwraca NULLPTR
     virtual std::shared_ptr<std::vector<SignalDatapoint>> GetFilteredData() = 0;
 
+    // Zwraca jeden ze statusów
     virtual Status GetStatus() = 0;
 
+    // Uruchamia filtrowanie wedle zadanego algorytmu. Zwraca prawdę jeżeli filtrowanie się udało.
+    // Po udanym wykonaniu tej metody, GetFilteredData() zwraca sensowne dane.
     virtual bool RunFiltering(FilterMethod method) = 0;
 };
 
