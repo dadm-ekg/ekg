769cfd2e010f27d251aaf3bdbe183de589dc19f7
Oliwia Rewer <oliwiarewer@MacBook-Pro-Oliwia.local>
2025-11-14 23:20:40 +0100

    GUI: dodanie plików UI, zmiana root h i cpp pod mainwindow.ui, wydaje sie ze git dziala, jakis testowy GUI

--- root.cpp
+++ root.cpp
@@ -1,12 +1,20 @@
 #include "root.h"
+#include "ui_mainwindow.h"
 #include "include/service/abstract/application_service.h"
 
 MainWindow::MainWindow(std::shared_ptr<IApplicationService> application_service, QWidget *parent)
-    : QMainWindow(parent), application_service_(std::move(application_service)) {
-    application_service_->Load("../../../../ludb/90.dat");
+    : QMainWindow(parent),
+    application_service_(std::move(application_service)),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);  // tu ładuje się mainwindow.ui
+
+    application_service_->Load("../../../../../ludb/90.dat");
 
     // TODO(Oliwia): rewiry Oliwii Rewer xd - trzeba dodać przyciski, itp
     // TODO(Sonia): obsługa wykresów
 }
 
-MainWindow::~MainWindow() = default;
+MainWindow::~MainWindow() {
+    delete ui;
+}
--- root.h
+++ root.h
@@ -2,17 +2,28 @@
 #define ROOT_H
 
 #include <QMainWindow>
+#include <memory>
 
 #include "include/service/abstract/application_service.h"
+
+
+class IApplicationService;
+
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
-    std::shared_ptr<IApplicationService> application_service_;
-
 public:
-    MainWindow(std::shared_ptr<IApplicationService> application_service, QWidget *parent = nullptr);
+    explicit MainWindow(std::shared_ptr<IApplicationService> application_service,
+                        QWidget *parent = nullptr);
+    ~MainWindow() override;
 
-    ~MainWindow();
+private:
+    std::shared_ptr<IApplicationService> application_service_;
+    Ui::MainWindow *ui;
 };
+
 #endif // ROOT_H
