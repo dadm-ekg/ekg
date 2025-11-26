cd70797902bd21cac4c962af75bf67dfe7cba2f6
JakuBN37 <kubanowak112@gmail.com>
2025-11-26 20:30:43 +0100

    add hrv1 module

--- include/service/abstract/hrv_time_processing_service.h
+++ include/service/abstract/hrv_time_processing_service.h
@@ -3,13 +3,24 @@
 #include <vector>
 
 #include "../../dto/hrv_time_metrics.h"
+#include "../../model/r_peaks_annotated_signal_datapoint.h"
 #include "../../model/signal_datapoint.h"
 
 class IHRVTimeProcessingService {
 public:
     virtual ~IHRVTimeProcessingService() = default;
 
-    virtual HRVTimeMetrics Process(const std::vector<SignalDatapoint>& datapoints, int frequency) = 0;
+    // Przetwarza sygnał EKG i wykryte piki R w celu obliczenia metryk czasowych i częstotliwościowych HRV
+    // datapoints - przefiltrowany sygnał EKG
+    // r_peaks - wykryte piki R (może być pusty wektor, wtedy metoda zwróci domyślne wartości)
+    // frequency - częstotliwość próbkowania sygnału
+    // method - metoda estymacji widma (Classic Periodogram, Lomb-Scargle, Welch)
+    virtual HRVTimeMetrics Process(
+        const std::vector<SignalDatapoint>& datapoints,
+        const std::vector<RPeaksAnnotatedSignalDatapoint>& r_peaks,
+        int frequency,
+        HRVTimeMetrics::SpectralMethod method = HRVTimeMetrics::SpectralMethod::CLASSIC_PERIODOGRAM
+    ) = 0;
 };
 
 #endif //EKG_HRV_TIME_PROCESSING_SERVICE_H
