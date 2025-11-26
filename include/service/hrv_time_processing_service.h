18d7cb05ff5fbfcb26bb5c8a9c290a13ba839ca7
JakuBN37 <kubanowak112@gmail.com>
2025-11-26 20:30:43 +0100

    add hrv1 module

--- include/service/hrv_time_processing_service.h
+++ include/service/hrv_time_processing_service.h
@@ -5,7 +5,12 @@
 
 class HRVTimeProcessingService : public IHRVTimeProcessingService {
 public:
-    HRVTimeMetrics Process(const std::vector<SignalDatapoint>& datapoints, int frequency) override;
+    HRVTimeMetrics Process(
+        const std::vector<SignalDatapoint>& datapoints,
+        const std::vector<RPeaksAnnotatedSignalDatapoint>& r_peaks,
+        int frequency,
+        HRVTimeMetrics::SpectralMethod method = HRVTimeMetrics::SpectralMethod::CLASSIC_PERIODOGRAM
+    ) override;
 };
 
 #endif //EKG_HRV_TIME_PROCESSING_SERVICE_IMPL_H
