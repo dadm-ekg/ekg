b8c7f8f20de43046bfaed9968c8db5d9950fc356
Jeremiasz <jeremiasz@MacBook-Air-Jeremiasz.local>
2025-11-26 20:53:56 +0100

    feat: dummy classifier

--- include/dto/heart_class_result.h
+++ include/dto/heart_class_result.h
@@ -1,8 +1,12 @@
 #ifndef EKG_HEART_CLASS_RESULT_H
 #define EKG_HEART_CLASS_RESULT_H
 
+#include <map>
+#include <string>
+
 class HeartClassResult {
-    // TODO(Jeremiasz): trzeba uzupełnić
+public:
+    std::map<int, std::string> annotations;
 };
 
-#endif //EKG_HEART_CLASS_RESULT_H
+#endif // EKG_HEART_CLASS_RESULT_H
