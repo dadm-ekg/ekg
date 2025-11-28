7cad26df5b32cf672b192f2fc9d25fad00e228f4
Mateusz Woźniak <matisiek11@gmail.com>
2025-11-28 23:05:56 +0100

    fix: signature

--- include/service/abstract/hrv_geo_processing_service.h
+++ include/service/abstract/hrv_geo_processing_service.h
@@ -1,12 +1,13 @@
 #ifndef EKG_HRV_GEO_PROCESSING_SERVICE_H
 #define EKG_HRV_GEO_PROCESSING_SERVICE_H
 #include "../../dto/hrv_geo_metrics.h"
+#include "../../model/r_peaks_annotated_signal_datapoint.h"
 #include <vector>
 
 class IHRVGeoProcessingService {
 public:
     virtual ~IHRVGeoProcessingService() = default;
-    virtual HRVGeoMetrics Process(const std::vector<double>& rr_intervals) = 0;
+    virtual HRVGeoMetrics Process(const std::vector<RPeaksAnnotatedSignalDatapoint>& datapoints, int frequency) = 0;
 };
 
 #endif //EKG_HRV_GEO_PROCESSING_SERVICE_H
